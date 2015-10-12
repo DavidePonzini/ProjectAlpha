be5c137e8b78d5ddb90024862838cda97e972a48
DavidePonzini <davide.ponzini95@gmail.com>
2015-10-12 11:19:42 +0200

    Initial version

---
+++ Source/ProjectAlpha/ProjectAlphaGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProjectAlphaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTALPHA_API AProjectAlphaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
