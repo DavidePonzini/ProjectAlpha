c65572df73b44cbb3a5a39e8f137977c92192051
DavidePonzini <davide.ponzini95@gmail.com>
2016-03-11 11:52:52 +0100

    Added custom trace channel

---
+++ Source/ProjectAlpha/Private/Inventory.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectAlpha.h"
+#include "Inventory.h"
+
+Inventory::Inventory()
+{
+}
+
+Inventory::~Inventory()
+{
+}
---
+++ Source/ProjectAlpha/Public/Types.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Engine/UserDefinedEnum.h"
+#include "Types.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTALPHA_API UTypes : public UUserDefinedEnum
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
